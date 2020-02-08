b1bc0b9cbfb3ab46864b93391454a8b4c4bf946b
pheobe <wscyy0510@163.com>
2020-02-08 15:58:20 +0800

    20200208

--- src/components/CompanyBase.vue
+++ src/components/CompanyBase.vue
@@ -94,6 +94,7 @@
             </el-tab-pane>
             <el-tab-pane label="企业预警" name="third">
               <div>
+                <div id="mountNode"></div>
 
             </div>
             </el-tab-pane>
@@ -109,12 +110,14 @@
 
 <script>
   import $ from 'jquery'
+  import G6 from '@antv/g6'
 
   export default {
         name: "CompanyBase",
       data(){
+
           return{
-            activeName2: 'second',
+            activeName2: 'third',
             li:this.$route.params.id,//可以返回当前企业id
             isCollapse: !false,
             figure:null,//接收后台的数据
@@ -149,6 +152,7 @@
       // alert($(".warn").text());
 
       this.init_radio();
+      this.init_node();
       if ($(".warn").text()=="僵尸企业") {
             $(".warn").addClass("isred");
           }
@@ -159,6 +163,38 @@
       },
 
     methods:{
+          init_node:function(){
+            const data = {
+              // 点集
+              nodes: [
+                {
+                  id: 'node1', // String，该节点存在则必须，节点的唯一标识
+                  x: 100, // Number，可选，节点位置的 x 值
+                  y: 200, // Number，可选，节点位置的 y 值
+                },
+                {
+                  id: 'node2', // String，该节点存在则必须，节点的唯一标识
+                  x: 300, // Number，可选，节点位置的 x 值
+                  y: 200, // Number，可选，节点位置的 y 值
+                },
+              ],
+              // 边集
+              edges: [
+                {
+                  source: 'node1', // String，必须，起始点 id
+                  target: 'node2', // String，必须，目标点 id
+                },
+              ],
+            };
+            const graph = new G6.Graph({
+              container: 'mountNode', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
+              width: 800, // Number，必须，图的宽度
+              height: 500, // Number，必须，图的高度
+            });
+            graph.data(data); // 读取 Step 2 中的数据源到图上
+            graph.render(); // 渲染图
+
+          },
       init_radio: function () {
         var echarts = require('echarts');
 
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import echarts from 'echarts';
 import './my-theme/index.css'
+
 Vue.prototype.$echarts = echarts;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
